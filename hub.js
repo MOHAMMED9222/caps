27d8044d73182248d43ea7fe853ca75bd1419f33
Mohammed Abubakar <122310719+MOHAMMED9222@users.noreply.github.com>
2023-05-29 12:44:19 -0700

    Create hub.js

---
+++ hub.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let eventPool = require('./eventPool');
+require('./vendor');
+require('./driver');
+var Chance = require('chance');
+var chance = new Chance();
+
+
+eventPool.on('pickup', (payload) => logger('pickup', payload));
+eventPool.on('in-transit', (payload) => logger('in-transit', payload));
+eventPool.on('delivered', (payload) => logger('delivered', payload));
+
+function logger(event, payload) {
+  console.log({
+    event,
+    time: new Date().toISOString(),
+    payload,
+  });
+}
+
+
+
+const start = () => {
+  setInterval(() => {
+    let store = chance.company();
+    eventPool.emit('VENDOR', store);
+  }, 5000);
+};
+
+start();
